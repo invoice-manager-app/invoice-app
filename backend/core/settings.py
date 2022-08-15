f649c7f2f783238abb87027983f1b0e37098d695
karamfarhan <www.karam777krm@gmail.com>
2022-08-15 20:46:48 +0300

    delete email

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -261,5 +261,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "dmax87545@gmail.com"
-EMAIL_HOST_PASSWORD = "eacylbecdebcgmcm"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
