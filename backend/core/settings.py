7a047a8c9f7f0add21b48be5a7aefb35ff491a9b
karamfarhan <www.karam777krm@gmail.com>
2022-11-16 16:40:07 +0300

    change database setting to sqlite

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -118,27 +118,27 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
-DB_NAME = "postgres"
-DB_USER = "postgres"
-DB_PASSWORD = "postgres"
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": DB_NAME,
-        "USER": DB_USER,
-        "PASSWORD": DB_PASSWORD,
-        "HOST": "pgdb",
-        "PORT": 5432,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+# DB_NAME = "postgres"
+# DB_USER = "postgres"
+# DB_PASSWORD = "postgres"
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": DB_NAME,
+#         "USER": DB_USER,
+#         "PASSWORD": DB_PASSWORD,
+#         "HOST": "pgdb",
+#         "PORT": 5432,
+#     }
+# }
 
 
 CHANNEL_LAYERS = {
@@ -252,4 +252,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "zahabiacompany@gmail.com"
-EMAIL_HOST_PASSWORD = "gppqpmvdtyroavia"
+EMAIL_HOST_PASSWORD = ""
